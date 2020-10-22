8c5472e80b6788b0a20d55ca0835017834ed93e5
Molly Lewis <mollyllewis@gmail.com>
2020-10-22 18:24:34 -0400

    pass on results and methods

--- writeups/paper/scripts/funnel_plot_helper.R
+++ writeups/paper/scripts/funnel_plot_helper.R
@@ -1,29 +1,26 @@
 library(tidyverse)
 
 generate_funnel_plot <- function(data){
-  
-  CRIT_95 = 1.96 
-  
 
-  ma_data <- data   
+  CRIT_95 = 1.96
 
-  # model <- rma.mv(d_calc ~ 1,  d_var_calc,  
-  #                 random = ~ 1 | short_cite/same_infant/x_1, 
-  #                 method = "REML", 
+  ma_data <- data
+
+  # model <- rma.mv(d_calc ~ 1,  d_var_calc,
+  #                 random = ~ 1 | short_cite/same_infant/x_1,
+  #                 method = "REML",
   #                 data=ma_data)
-  # 
+  #
   # predicted_val <- predict.rma(model)
-  
-  
-  
-ma_funnel <- ma_data %>% 
+
+ma_funnel <- ma_data %>%
   mutate(
     se = sqrt(d_var_calc),
     es = d_calc,
     center = mean(d_calc),
     lower_lim = max(se) + .05 * max(se),
     type = "non_moderated"
-  ) %>%  
+  ) %>%
   select(es, se, center, lower_lim)
 
 
@@ -49,19 +46,18 @@ funnel95_data_Y = funnel_shape_wide  %>%
 
 funnel95.data = bind_cols(funnel95_data_x, funnel95_data_Y)
 
-
 ggplot(ma_funnel, aes(x = es, y = -se)) +
-  xlab("Effect Size (Cohen's d)")  +
+  xlab(expression(paste("Effect Size (Cohen's ", italic(d), ")")))  +
   ylab("Standard Error\n")  +
   geom_polygon(aes(x = x, y = y),
                data = funnel95.data,
-               fill = "grey") +
+               fill = "grey90") +
   geom_vline(aes(xintercept=x2),
              linetype = "dashed", color = "red", size = .8, data = funnel_shape_wide) +
   geom_vline(xintercept = 0, linetype = "dashed", color = "grey44",  size = .8) +
   scale_y_continuous(labels = function(x){abs(x)}) +
-  geom_point(data = ma_funnel, size = 4) +
-  theme(text = element_text(size=10),
+  geom_point(data = ma_funnel, size = 1.5) +
+  theme(text = element_text(size = 11),
         panel.grid.major = element_line(colour = "white", size = 0.2),
         panel.grid.minor = element_line(colour = "white", size = 0.5),
         strip.text.x = element_text(size = 9),
