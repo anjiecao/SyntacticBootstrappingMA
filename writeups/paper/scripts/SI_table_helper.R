be5bcf73f644d3e48a7bd0e75a535ede1d538a3f
anjie cao <anjiecao@stanford.edu>
2020-10-29 13:31:35 -0700

    reorg sec1 in SI

---
+++ writeups/paper/scripts/SI_table_helper.R
@@ -0,0 +1,48 @@
+
+pretty_moderator_name <- function(s){
+  new_s <- str_to_title(gsub("_", " ",s))
+  return(new_s)
+}
+
+convert_pretty_print_table <- function(full_df, current_moderator){
+  
+  full_df <- full_df %>% 
+    select(this_moderator, n, 
+           estimate_print_full, z_print, p_print, 
+           mod_estimate_print_full, mod_CI_print, mod_z_print, mod_p_print) %>% 
+    mutate(
+      intercept = estimate_print_full, 
+      moderator = mod_estimate_print_full,
+      intercept_z_val = gsub("= ","", z_print), 
+      intercept_p_val = gsub("= ", "", p_print),
+      mod_z_val = gsub("= ", "", mod_z_print),
+      mod_p_val = gsub("= ", "", mod_p_print)
+    )
+  
+  
+  estimates <- full_df %>% 
+    filter(this_moderator == current_moderator) %>% 
+    pivot_longer(cols = c(intercept, moderator), names_to = "Parameter", values_to = "Estimate") %>% 
+    select(Parameter, Estimate)
+  
+  z_val <- full_df %>% 
+    filter(this_moderator == current_moderator) %>% 
+    pivot_longer(cols = c(intercept_z_val, mod_z_val), names_to = "Parameter", values_to = "z value") %>% 
+    select("z value")
+  
+  p_val <- full_df %>% 
+    filter(this_moderator == current_moderator) %>% 
+    pivot_longer(cols = c(intercept_p_val, mod_p_val), names_to = "Parameter", values_to = "p value") %>% 
+    select("p value")
+  
+  all_df <- bind_cols(estimates, z_val, p_val) %>% 
+    mutate(
+      Parameter = case_when(
+        Parameter == "moderator" ~ pretty_moderator_name(current_moderator),
+        Parameter == "intercept" ~ "Intercept"
+      )
+    )
+  
+  return(all_df)
+  
+}
